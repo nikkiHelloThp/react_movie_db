0e11e1f895e5d0d4b34412d3ba74c8dbf2183fa2
nikkiHelloThp <nicolas.stanojevic.lpo@gmail.com>
2019-12-10 22:03:39 +0100

    Updates

---
+++ precache-manifest.465c2596147ec38b38a5ce4b7c8ad004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeeb920382a4654944d121e9b423c7d",
+    "url": "/react_movie_db/index.html"
+  },
+  {
+    "revision": "cb807c6546a6077148e5",
+    "url": "/react_movie_db/static/css/main.6ce0d6bf.chunk.css"
+  },
+  {
+    "revision": "a58df1464aa907055121",
+    "url": "/react_movie_db/static/js/2.6e86b987.chunk.js"
+  },
+  {
+    "revision": "cb807c6546a6077148e5",
+    "url": "/react_movie_db/static/js/main.4b7ba230.chunk.js"
+  },
+  {
+    "revision": "ed69731556c8e00e9247",
+    "url": "/react_movie_db/static/js/runtime-main.fd5b72d1.js"
+  },
+  {
+    "revision": "02a9430b88975cae16fcfcc9cf7b5799",
+    "url": "/react_movie_db/static/media/movie_logo.02a9430b.svg"
+  }
+]);
